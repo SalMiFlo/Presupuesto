ba61912567122e0bcd374ab105ea37c82ef813e8
SalMiFlo <134639794+SalMiFlo@users.noreply.github.com>
2023-06-11 15:06:07 -0600

    Add files via upload

--- Presupuesto/Program.cs
+++ Presupuesto/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+using Presupuesto.Models;
 using Presupuesto.Servicios;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,30 @@ builder.Services.AddTransient<IRepositorioServiciosUsuarios, RepositorioServicio
 builder.Services.AddTransient<IRepositorioCuentas, RepositorioCuentas>();
 builder.Services.AddTransient<IRepositorioCategorias, RepositorioCategorias>();
 builder.Services.AddTransient<IRepositorioTransacciones, RepositorioTransacciones>();
+builder.Services.AddTransient<ISReportes, SReportes>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<IRepositorioUsuarios, RepositorioUsuarios>();
 builder.Services.AddAutoMapper(typeof(Program)); //Centralizar la lógica de copiado de datos entre objetos.
+builder.Services.AddTransient<SignInManager<Usuario>>(); //Inyectar para el manejo de usuarios con SignInManager.
+//Servicio para IUSerStore
+builder.Services.AddTransient<IUserStore<Usuario>, UsuarioStore>();
+//Dentro del parentesis especificas las reglas para las validaciones si no necesitas las predeterminadas
+builder.Services.AddIdentityCore<Usuario>(opciones =>
+{
+    opciones.Password.RequireDigit = false;
+    opciones.Password.RequireLowercase = false;
+    opciones.Password.RequireUppercase = false;
+    opciones.Password.RequireNonAlphanumeric = false;
+}).AddErrorDescriber<MensajesDeErrorIdentity>();
+//Servicio para IUSerStore
+//Uso de cookies para autenticación
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultChallengeScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignOutScheme = IdentityConstants.ApplicationScheme;
+}).AddCookie(IdentityConstants.ApplicationScheme);
+//Uso de cookies para autenticación
 
 var app = builder.Build();
 
@@ -25,6 +50,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+//Uso de cookies para autenticación
+app.UseAuthentication();
+//Uso de cookies para autenticación
 
 app.UseAuthorization();
 
